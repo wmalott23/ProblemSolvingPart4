5e3545656db547c814227433c11f820aad58a6fd
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-03-15 08:47:17 -0700

    added main.py

---
+++ ProblemSolvingPart4.py
@@ -0,0 +1,22 @@
+# Reversing a string
+
+# def rev_string():
+#     rev_list = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15]
+#     let_input = []
+#     user_input = input("What word would you like reversed?")
+#     user_len = len(user_input)
+#     for letter in user_input:
+#         let_input.append(letter)
+#     for rev_let in rev_list:
+#         if rev_let >= -(user_len):
+#             print(let_input[rev_let])
+
+# rev_string()
+
+# Capitalize letter
+
+def cap_list():
+    cap_input = input("What sentence would you like to capitalize each letter of?")
+    cap_input.find(" ")
+
+cap_list()
